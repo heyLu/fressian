b05f501cec226c7597d4d4c681f4318d9811e8ff
Lucas Stadler <lu@papill0n.org>
2015-03-12 13:41:46 +0100

    add clojure example to write example.fressian
    `example.frsn`? or even `example.fsn`?

---
+++ example.clj
@@ -0,0 +1,9 @@
+(import '(java.io File FileInputStream FileOutputStream))
+(import '(org.fressian FressianReader FressianWriter))
+
+(def f (File. "example.fressian"))
+;(def r (FressianReader. (FileInputStream. f)))
+(def w (FressianWriter. (FileOutputStream. f)))
+
+;(.writeObject w [1 2 3 4 5])
+(.writeObject w [1 2 3 4 5 "hello"])
