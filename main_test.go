2f3ed8719be7ca74c1d32fc9887be6f7c86172bd
Lucas Stadler <lu@papill0n.org>
2015-03-12 15:54:25 +0100

    use gol's util/testing

--- main_test.go
+++ main_test.go
@@ -3,15 +3,13 @@ package main
 import (
 	"bytes"
 	"testing"
+
+	tu "github.com/klingtnet/gol/util/testing"
 )
 
 func TestReadInt(t *testing.T) {
 	r := NewReader(bytes.NewReader([]byte{0x01}))
 	i := r.readInt()
-	if r.Err() != nil {
-		t.Fatal(r.Err())
-	}
-	if i != 1 {
-		t.Errorf("%d != %d", i, 1)
-	}
+	tu.RequireNil(t, r.Err())
+	tu.ExpectEqual(t, i, 1)
 }
