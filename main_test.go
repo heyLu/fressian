7c6987d9c549dbe1c4421e3a73ba83c954064cc4
Lucas Stadler <lu@papill0n.org>
2015-03-12 15:58:40 +0100

    add more test cases for readInt

--- main_test.go
+++ main_test.go
@@ -9,6 +9,14 @@ import (
 
 func TestReadInt(t *testing.T) {
 	expectReadInt(t, []byte{0x01}, 1)
+	expectReadInt(t, []byte{0x02}, 2)
+	expectReadInt(t, []byte{0x03}, 3)
+	expectReadInt(t, []byte{0x04}, 4)
+	expectReadInt(t, []byte{0x05}, 5)
+	expectReadInt(t, []byte{0x06}, 6)
+	expectReadInt(t, []byte{0x07}, 7)
+	expectReadInt(t, []byte{0x08}, 8)
+	expectReadInt(t, []byte{0x09}, 9)
 }
 
 func expectReadInt(t *testing.T, bs []byte, res int) {
