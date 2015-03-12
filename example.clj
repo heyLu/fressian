522fd0dce56545edcab940a0f945dbd7b6505055
Lucas Stadler <lu@papill0n.org>
2015-03-12 18:17:26 +0100

    add number writing examples
    they're for debugging, but why not let other people see them as well?
    
    [this commit was sponsored by another spurt of "keep your commits small"
    extremism.]

--- example.clj
+++ example.clj
@@ -5,6 +5,9 @@
 ;(def r (FressianReader. (FileInputStream. f)))
 (def w (FressianWriter. (FileOutputStream. f)))
 
+;(.writeObject w 89)
+;(.writeObject w 33554431)
+
 ;(.writeObject w [1 2 3 4 5])
 (.writeObject w [1 2 3 4 5 "hello"])
 
