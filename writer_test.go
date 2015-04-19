05a90bcfc6671c20e6e4da4a7f819ffd65ae6964
Lucas Stadler <lu@papill0n.org>
2015-04-19 17:53:16 +0200

    order tests like they are in writer.go

--- writer_test.go
+++ writer_test.go
@@ -7,6 +7,25 @@ import (
 	tu "github.com/klingtnet/gol/util/testing"
 )
 
+func TestWriteBool(t *testing.T) {
+	testWriteBool(t, true)
+	testWriteBool(t, false)
+}
+
+func testWriteBool(t *testing.T, b bool) {
+	buf := new(bytes.Buffer)
+	w := NewWriter(buf, nil)
+	w.WriteBool(b)
+	w.Flush()
+
+	tu.ExpectNil(t, w.Error())
+
+	r := NewReader(buf, nil)
+	val, err := r.ReadObject()
+	tu.ExpectNil(t, err)
+	tu.ExpectEqual(t, b, val)
+}
+
 func TestWriteInt(t *testing.T) {
 	testWriteInt(t, 0)
 	testWriteInt(t, 1)
@@ -27,23 +46,4 @@ func testWriteInt(t *testing.T, i int) {
 	val, err := r.ReadObject()
 	tu.ExpectNil(t, err)
 	tu.ExpectEqual(t, i, val)
-}
-
-func TestWriteBool(t *testing.T) {
-	testWriteBool(t, true)
-	testWriteBool(t, false)
-}
-
-func testWriteBool(t *testing.T, b bool) {
-	buf := new(bytes.Buffer)
-	w := NewWriter(buf, nil)
-	w.WriteBool(b)
-	w.Flush()
-
-	tu.ExpectNil(t, w.Error())
-
-	r := NewReader(buf, nil)
-	val, err := r.ReadObject()
-	tu.ExpectNil(t, err)
-	tu.ExpectEqual(t, b, val)
 }
