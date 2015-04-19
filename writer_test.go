9d81aaf3f7a447e9ffd66c5572728081453208c1
Lucas Stadler <lu@papill0n.org>
2015-04-19 17:37:40 +0200

    actually test WriteInt and WriteBool

--- writer_test.go
+++ writer_test.go
@@ -2,22 +2,48 @@ package fressian
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
+
+	tu "github.com/klingtnet/gol/util/testing"
 )
 
-func TestWriteBackAndForth(t *testing.T) {
-	buf := new(bytes.Buffer)
-	writer := NewWriter(buf, nil)
-	writer.WriteInt(124124214211)
-	//writer.WriteInt(1023)
-	//writer.WriteInt(583)
-	writer.Flush()
-	fmt.Println(writer.Error())
-	fmt.Printf("%#v\n", buf.Bytes())
+func TestWriteInt(t *testing.T) {
+	testWriteInt(t, 0)
+	testWriteInt(t, 1)
+	testWriteInt(t, 10)
+	testWriteInt(t, 583)
+	testWriteInt(t, 36342523521)
+}
 
-	reader := NewReader(buf, nil)
-	val, err := reader.ReadObject()
-	fmt.Println(err)
-	fmt.Println(val)
+func testWriteInt(t *testing.T, i int) {
+	buf := new(bytes.Buffer)
+	w := NewWriter(buf, nil)
+	w.WriteInt(i)
+	w.Flush()
+
+	tu.ExpectNil(t, w.Error())
+
+	r := NewReader(buf, nil)
+	val, err := r.ReadObject()
+	tu.ExpectNil(t, err)
+	tu.ExpectEqual(t, i, val)
+}
+
+func TestWriteBool(t *testing.T) {
+	testWriteBool(t, true)
+	testWriteBool(t, false)
+}
+
+func testWriteBool(t *testing.T, b bool) {
+	buf := new(bytes.Buffer)
+	w := NewWriter(buf, nil)
+	w.WriteBool(b)
+	w.Flush()
+
+	tu.ExpectNil(t, w.Error())
+
+	r := NewReader(buf, nil)
+	val, err := r.ReadObject()
+	tu.ExpectNil(t, err)
+	tu.ExpectEqual(t, b, val)
 }
