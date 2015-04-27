84126e803161ef473b42989e231c5d8c27c4d058
Lucas Stadler <lu@papill0n.org>
2015-04-27 15:52:59 +0200

    add two unicode string test cases

--- writer_test.go
+++ writer_test.go
@@ -56,6 +56,8 @@ func TestWriteValue(t *testing.T) {
 	testWriteValue(t, 3)
 	//testWriteValue(t, []int{1, 2, 3})
 	testWriteValue(t, "hello")
+	testWriteValue(t, "hällo")
+	testWriteValue(t, "日本語")
 	testWriteValue(t, "Hello, World!")
 	testWriteValue(t, Key{"hello", "world"})
 	testWriteValue(t, []interface{}{1, 2, true, 4})
